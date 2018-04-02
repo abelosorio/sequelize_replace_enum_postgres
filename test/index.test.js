2ac18fdfc64688140b61a98ba1762280e541d8e7
Vaidotas Sm <vaidotas.sm@gmail.com>
2018-04-02 19:19:25 +0300

    Introduce eslint to fit existing code
    Also adjust new test code

--- test/index.test.js
+++ test/index.test.js
@@ -1,69 +1,67 @@
 'use strict';
+/* eslint-disable require-jsdoc */
 
-import mocha from 'mocha';
 import assert from 'assert';
-import { removeWhiteSpace } from './utils/testUtils'
+import { removeWhiteSpace } from './utils/testUtils';
 import replaceEnum from '../src/index';
 
 describe('replaceEnum() - enum replacement:', () => {
+  it('should pass correct queries to queryInterface', async () => {
+    const queryInterface = queryInterfaceMock();
+    await replaceEnum({
+      queryInterface,
+      tableName: 'table1',
+      columnName: 'column1',
+      defaultValue: 'A',
+      newValues: ['A', 'B', 'C'],
+      enumName: 'enum1'
+    });
 
-	it('should pass correct queries to queryInterface', async () => {
-		const queryInterface = QueryInterfaceMock();
-		await replaceEnum({
-			queryInterface,
-			tableName: 'table1',
-			columnName: 'column1',
-			defaultValue: 'A',
-			newValues: ['A', 'B', 'C'],
-			enumName: 'enum1'
-		});
+    assert.deepEqual(queryInterface.getQueries().map((q) => removeWhiteSpace(q.sql)), [
+      `CREATE TYPE "enum1_new" AS ENUM (\'A\', \'B\', \'C\')`,
+      `ALTER TABLE "table1" ALTER COLUMN "column1" DROP DEFAULT`,
+      ` ALTER TABLE "table1" ALTER COLUMN "column1" TYPE "enum1_new" USING ("column1"::text::"enum1_new") `,
+      `DROP TYPE "enum1"`,
+      `ALTER TYPE "enum1_new" RENAME TO "enum1"`,
+      ` ALTER TABLE "table1" ALTER COLUMN "column1" SET DEFAULT 'A'::"enum1" `,
+    ]);
+  });
 
-		assert.deepEqual(queryInterface.getQueries().map(q => removeWhiteSpace(q.sql)), [
-			`CREATE TYPE "enum1_new" AS ENUM (\'A\', \'B\', \'C\')`,
-			`ALTER TABLE "table1" ALTER COLUMN "column1" DROP DEFAULT`,
-			` ALTER TABLE "table1" ALTER COLUMN "column1" TYPE "enum1_new" USING ("column1"::text::"enum1_new") `,
-			`DROP TYPE "enum1"`,
-			`ALTER TYPE "enum1_new" RENAME TO "enum1"`,
-			` ALTER TABLE "table1" ALTER COLUMN "column1" SET DEFAULT 'A'::"enum1" `,
-		]);
-	});
+  it('should pass correct options - transaction', async () => {
+    const queryInterface = queryInterfaceMock();
+    await replaceEnum({
+      queryInterface,
+      tableName: 'table1',
+      columnName: 'column1',
+      defaultValue: 'A',
+      newValues: ['A', 'B', 'C'],
+      enumName: 'enum1'
+    });
 
-	it('should pass correct options - transaction', async () => {
-		const queryInterface = QueryInterfaceMock();
-		await replaceEnum({
-			queryInterface,
-			tableName: 'table1',
-			columnName: 'column1',
-			defaultValue: 'A',
-			newValues: ['A', 'B', 'C'],
-			enumName: 'enum1'
-		});
-
-		const queries = queryInterface.getQueries();
-		assert.equal(queries.length, 6, 'should create 6 queries');
-		for (const query of queries) {
-			assert.deepEqual(query.options, { transaction: { mockTransaction: true } });
-		}
-	});
+    const queries = queryInterface.getQueries();
+    assert.equal(queries.length, 6, 'should create 6 queries');
+    for (const query of queries) {
+      assert.deepEqual(query.options, { transaction: { mockTransaction: true } });
+    }
+  });
 });
 
+function queryInterfaceMock() {
+  const queries = [];
 
-function QueryInterfaceMock() {
-	const queries = [];
-
-	return {
-		sequelize: {
-			query(sql, options) {
-				queries.push({ sql, options });
-				return Promise.resolve();
-			},
-			async transaction(callback) {
-				await callback({ mockTransaction: true });
-				return Promise.resolve();
-			}
-		},
-		getQueries() {
-			return queries;
-		}
-	};
+  return {
+    sequelize: {
+      query(sql, options) {
+        queries.push({ sql, options });
+        return Promise.resolve();
+      },
+      async transaction(callback) {
+        await callback({ mockTransaction: true });
+        return Promise.resolve();
+      }
+    },
+    getQueries() {
+      return queries;
+    }
+  };
 }
--- test/queries.test.js
+++ test/queries.test.js
@@ -1,47 +1,46 @@
-import mocha from 'mocha';
 import assert from 'assert';
-import { removeWhiteSpace } from './utils/testUtils'
+import { removeWhiteSpace } from './utils/testUtils';
 import {
-	getQueryToCreateEnum,
-	getQueryToDropEnum,
-	getQueryToRemoveDefaultFromColumn,
-	getQueryToRenameEnum,
-	getQueryToSetEnumType,
-	getQueryToSetColumnDefault
+  getQueryToCreateEnum,
+  getQueryToDropEnum,
+  getQueryToRemoveDefaultFromColumn,
+  getQueryToRenameEnum,
+  getQueryToSetEnumType,
+  getQueryToSetColumnDefault
 } from '../src/index';
 
 describe('queries:', () => {
-	it('getQueryToCreateEnum()', () => {
-		const sql = getQueryToCreateEnum('enum1', ['A', 'B']);
+  it('getQueryToCreateEnum()', () => {
+    const sql = getQueryToCreateEnum('enum1', ['A', 'B']);
 
-		assert.equal(sql, `CREATE TYPE "enum1" AS ENUM ('A', 'B')`);
-	});
+    assert.equal(sql, `CREATE TYPE "enum1" AS ENUM ('A', 'B')`);
+  });
 
-	it('getQueryToDropEnum()', () => {
-		const sql = getQueryToDropEnum('enum1');
-		assert.equal(sql, `DROP TYPE "enum1"`);
-	});
+  it('getQueryToDropEnum()', () => {
+    const sql = getQueryToDropEnum('enum1');
+    assert.equal(sql, `DROP TYPE "enum1"`);
+  });
 
-	it('getQueryToRemoveDefaultFromColumn()', () => {
-		const sql = getQueryToRemoveDefaultFromColumn('enum1', 'column1');
-		assert.equal(sql, `ALTER TABLE "enum1" ALTER COLUMN "column1" DROP DEFAULT`);
-	});
+  it('getQueryToRemoveDefaultFromColumn()', () => {
+    const sql = getQueryToRemoveDefaultFromColumn('enum1', 'column1');
+    assert.equal(sql, `ALTER TABLE "enum1" ALTER COLUMN "column1" DROP DEFAULT`);
+  });
 
-	it('getQueryToRenameEnum()', () => {
-		const sql = getQueryToRenameEnum('enum1', 'enumNew1');
-		assert.equal(sql, `ALTER TYPE "enum1" RENAME TO "enumNew1"`);
-	});
+  it('getQueryToRenameEnum()', () => {
+    const sql = getQueryToRenameEnum('enum1', 'enumNew1');
+    assert.equal(sql, `ALTER TYPE "enum1" RENAME TO "enumNew1"`);
+  });
 
-	it('getQueryToSetEnumType()', () => {
-		const sql = removeWhiteSpace(getQueryToSetEnumType('table1', 'column1', 'enum1'));
-		assert.equal(
-			sql,
-			` ALTER TABLE "table1" ALTER COLUMN "column1" TYPE "enum1" USING ("column1"::text::"enum1") `
-		);
-	});
+  it('getQueryToSetEnumType()', () => {
+    const sql = removeWhiteSpace(getQueryToSetEnumType('table1', 'column1', 'enum1'));
+    assert.equal(
+      sql,
+      ` ALTER TABLE "table1" ALTER COLUMN "column1" TYPE "enum1" USING ("column1"::text::"enum1") `
+    );
+  });
 
-	it('getQueryToSetColumnDefault()', () => {
-		const sql = removeWhiteSpace(getQueryToSetColumnDefault('table1', 'column1', 'defaultVal1', 'STRING'));
-		assert.equal(sql, ` ALTER TABLE "table1" ALTER COLUMN "column1" SET DEFAULT 'defaultVal1'::"STRING" `);
-	});
+  it('getQueryToSetColumnDefault()', () => {
+    const sql = removeWhiteSpace(getQueryToSetColumnDefault('table1', 'column1', 'defaultVal1', 'STRING'));
+    assert.equal(sql, ` ALTER TABLE "table1" ALTER COLUMN "column1" SET DEFAULT 'defaultVal1'::"STRING" `);
+  });
 });
